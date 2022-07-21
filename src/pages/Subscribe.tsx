55da10d1e06b5f1fb4dc611d57d7cd7f1e5a3e74
Willians Ferreira <williansdesigner@hotmail.com>
2022-07-21 11:33:24 -0300

    correcai link da imagem da home

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import CodeMockupImg from '../assets/code-mockup.png';
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
@@ -67,7 +68,7 @@ export function Subscribe() {
                 </div>
             </div>
             
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={CodeMockupImg} className="mt-10" alt="" />
         </div>
     )
 }
